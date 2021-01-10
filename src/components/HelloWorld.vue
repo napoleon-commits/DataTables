685cc747677a6e49f3a50f83e70384b5504e1ae3
Byron Hall <byronhall@MacBook-Pro.attlocal.net>
2021-01-10 15:54:53 -0600

    added table as in app

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,55 @@
 <template>
   <v-container>
-    <table id="table_id" class="display">
-      <thead>
-        <tr>
-          <th>Column 1</th>
-          <th>Column 2</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Row 1 Data 1</td>
-          <td>Row 1 Data 2</td>
-        </tr>
-        <tr>
-          <td>Row 2 Data 1</td>
-          <td>Row 2 Data 2</td>
-        </tr>
-      </tbody>
-    </table>
+    <v-dialog>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          v-bind="attrs"
+          v-on="on"
+        >
+          DataTable table
+        </v-btn>
+      </template>
+      <v-list>
+          <v-list-item>
+            <v-list-item-title class="text-wrap">
+              <span>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum,
+                tortor eu rhoncus lobortis, velit urna lacinia dui, eget blandit justo diam vitae nibh. 
+                Pellentesque aliquet laoreet pretium. Quisque hendrerit tellus ut nisl feugiat, a laoreet est luctus.
+              </span>
+              <div class="mt-3">
+                <StaticChessBoard
+                  pieceString="rnbqkbnrpppppppp................................PPPPPPPPRNBQKBNR"
+                />
+              </div>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+    </v-dialog>
   </v-container>
 </template>
 
 <script>
   window.$ = window.jquery = require('jquery');
   window.dt = require('datatables.net');
+  import StaticChessBoard from '@/components/subcomponents/StaticChessBoard';
 
   export default {
     name: 'HelloWorld',
+
+    components: {
+      StaticChessBoard,
+    },
 
     data: () => ({
     }),
 
     mounted(){
-      window.$('#table_id').DataTable({
-        bPaginate: false,
-        bInfo: false,
-        searching: false,
-      });
+      // window.$('#table_id').DataTable({
+      //   bPaginate: false,
+      //   bInfo: false,
+      //   searching: false,
+      // });
     }
   }
 </script>
---
+++ src/components/subcomponents/StaticChessBoard.vue
@@ -0,0 +1,80 @@
+<template>
+    <table class="ma-auto" :style="`border: 2px solid ${borderColor}; backgroundColor: rgba(0,0,0,0.87);`">
+        <tr v-for="(n, rank) in 8" :key="rank">
+            <td
+                v-for="(n, file) in 8" 
+                :key="file" 
+                :style="`
+                    backgroundColor: ${
+                    ((rank+file)%2===1)?'#FFF':customColor
+                    } !important;
+                `"
+            >
+                <div
+                    v-if="pieceString[rank*8+file]==='x' || pieceString[rank*8+file]==='o'"
+                    class="text-xl-h3 text-lg-h3 text-md-h3 text-sm-h3 text-subtitle-2"
+                    style="color: rgba(0,0,0,0.87); text-align: center;"
+                >
+                    <span v-if="pieceString[rank*8+file]==='x'">&times;</span>
+                    <span v-else>&bull;</span>
+                </div>
+                <ChessPieceImage
+                    :letter="pieceString[rank*8+file]"
+                    :maxWidth="windowWidth*(goldenRatio)/8"
+                    :maxHeight="windowHeight*(goldenRatio)/8"
+                    v-else
+                />
+            </td>
+        </tr>
+    </table>
+</template>
+
+<script>
+import ChessPieceImage from '@/components/subcomponents/ChessPieceImage';
+import goldenRatio from '@/static/GoldenRatio';
+
+export default {
+    name: 'StaticChessBoard',
+    props: ['pieceString'],
+    components:{
+        ChessPieceImage,
+    },
+    data(){
+        return {
+            windowHeight: window.innerHeight,
+            windowWidth: window.innerWidth,
+            goldenRatio,
+        }
+    },
+    computed: {
+        customColor(){
+            return 'grey';
+        },
+        borderColor(){
+            if(this.$vuetify.theme.dark){
+                return 'white';
+            }
+            return 'grey';
+        }
+    },
+    methods: {
+        onResize() {
+            this.windowHeight = window.innerHeight;
+            this.windowWidth = window.innerWidth;
+        }
+    },
+    mounted() {
+        this.$nextTick(() => {
+            window.addEventListener('resize', this.onResize);
+        });
+    },
+
+    beforeDestroy() { 
+        window.removeEventListener('resize', this.onResize); 
+    },
+}
+</script>
+
+<style>
+
+</style>
